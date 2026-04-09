4924e694d5b876398c3cdf9caf144b1f0f9661d9
Vanessa V. <redhotchiligal@yahoo.com>
2026-04-09 11:56:21 -0400

    remove languages that i dont use

--- csharp/README.md
+++
@@ -1,4 +0,0 @@
-Yatzy Refactoring Kata
-======================
-
-For exercise instructions see [top level README](../README.md)
--- scala/src/main/scala/Yatzy/YatzyCalculator.scala
+++
@@ -1,7 +0,0 @@
-package yatzy
-
-trait YatzyCalculator {
-  def validCategories: List[String]
-
-  def score(dice: List[Int], category: String): Int
-}
